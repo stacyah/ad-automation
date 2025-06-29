d9d0fbb724f9252c59c4ce306b7708d8a3e1558b
stacyah <stacyhilliard457@email.com>
2025-06-29 03:36:46 -0500

    Add scripts to automate AD user creation and deactivation

---
+++ Disable-User.ps1
@@ -0,0 +1,20 @@
+﻿# accept username as an argument
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$Username
+)
+
+Import-Module ActiveDirectory
+
+$disabledOUPath = "OU=DisabledUsers,DC=corp,DC=net"
+$user = Get-ADUser -Identity $Username
+
+if ($user) {
+    Disable-ADAccount -Identity $user
+    Write-Host "disabled account: $Username"
+
+    Move-ADObject -Identity $user.DistinguishedName -TargetPath $disabledOUPath
+    Write-Host "moved $Username to DisabledUsers OU"
+} else {
+    Write-Host "error: user '$Username' not found"
+}
---
+++ New-Hires.ps1
@@ -0,0 +1,30 @@
+﻿# import active directory module
+Import-Module ActiveDirectory
+
+$csvPath = "C:\Users\Administrator\Documents\new_users.csv"
+$ouPath = "OU=CorpUsers,DC=corp,DC=net"
+$usersToCreate = Import-Csv -Path $csvPath
+
+# loop through csv and create users
+foreach ($user in $usersToCreate) {
+    $firstName  = $user.FirstName
+    $lastName   = $user.LastName
+    $department = $user.Department
+    $jobTitle   = $user.JobTitle
+    $username = "$($firstName.Substring(0,1))$lastName".ToLower()
+    $defaultPassword = "Password123!" | ConvertTo-SecureString -asPlainText -Force
+
+    New-ADUser -Name "$firstName $lastName" `
+        -GivenName $firstName `
+        -Surname $lastName `
+        -SamAccountName $username `
+        -UserPrincipalName "$username@corp.net" `
+        -Path $ouPath `
+        -Department $department `
+        -Title $jobTitle `
+        -AccountPassword $defaultPassword `
+        -Enabled $true `
+        -ChangePasswordAtLogon $true
+
+    Write-Host "created user: $username"
+}
